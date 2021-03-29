655aa3609dd7eb65253e2b4d17a9cd7f083e5807
Vagner Oliveira <ziminny@gmail.com>
2021-03-29 15:00:25 -0300

    mail

--- dist/configs/mail.js
+++ dist/configs/mail.js
@@ -4,23 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var nodemailer_1 = __importDefault(require("nodemailer"));
-var aws_sdk_1 = __importDefault(require("aws-sdk"));
-aws_sdk_1.default.config.update({
-    accessKeyId: 'ses-smtp-user.20210329-130604',
-    secretAccessKey: 'BIkh/Wo126+OcUiS37lfhlP4MWOTpCQD9RrtAWBcbt6z',
-    region: 'sa-east-1'
-});
 exports.default = nodemailer_1.default.createTransport({
-    SES: new aws_sdk_1.default.SES({
-        apiVersion: '2021-12-03'
-    })
-    // host: "smtp.gmail.com",
-    // port: 465,
-    // secure: true,
-    // auth: {
-    //   user: process.env.USER_EMAIL_LOGIN, 
-    //   pass: process.env.USER_EMAIL_PASSWORD     
-    // }
+    // service:process.env.EMAIL_SERVICE,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.USER_EMAIL_LOGIN,
+        pass: process.env.USER_EMAIL_PASSWORD
+    }
 });
 // export default nodemailer.createTransport({
 //   host: "smtp.mailtrap.io",
